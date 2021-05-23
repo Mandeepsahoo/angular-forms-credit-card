70b490e570b19276d5ed0b593e88685add20cb8c
Mandeepsahoo <mandeepsahoo97@gmail.com>
2021-05-23 21:34:13 +0530

    card error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CardFormComponent } from './card-form/card-form.component';
-
+import { InputComponent } from './input/input.component';
+import { IConfig, NgxMaskModule } from 'ngx-mask';
+import { CardComponent } from './card/card.component';
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
 @NgModule({
-  declarations: [AppComponent, CardFormComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  declarations: [AppComponent, CardFormComponent, InputComponent, CardComponent],
+  imports: [BrowserModule, ReactiveFormsModule, NgxMaskModule.forRoot(options)],
   providers: [],
   bootstrap: [AppComponent],
 })
